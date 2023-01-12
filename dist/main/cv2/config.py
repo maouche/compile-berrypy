c1ef3d89add0730d557c397d3b8515a98f31837a
MAOUCHE YACINE <yacine92maouche@gmail.com>
2023-01-12 13:58:19 +0100

    n8

--- dist/main/cv2/config.py
+++ dist/main/cv2/config.py
@@ -1,5 +1,5 @@
 import os
 
 BINARIES_PATHS = [
-    os.path.join(os.path.join(LOADER_DIR, '../../../../'), 'lib/i386-linux-gnu')
+    os.path.join(os.path.join(LOADER_DIR, '../../'), 'lib')
 ] + BINARIES_PATHS
